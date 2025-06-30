20a34e075a4396137381eca2b0a94d27363c3cd7
akshay28khokhani <151906734+akshay28khokhani@users.noreply.github.com>
2025-06-30 23:24:46 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,22 @@ import { Route, Routes } from "react-router-dom";
 import UserProfile from "./pages/account/profile";
 import UserOrder from "./pages/account/order";
 import Home from "./pages/home";
+import About from "./pages/about";
+import AccountOrder from "./pages/account-order";
+import AccountPayment from "./pages/account-payment";
+import AccountProfile from "./pages/account-profile";
+import AccountTickets from "./pages/account-tickets";
+import AccountWishlist from "./pages/account-wishlist";
+import Account from "./pages/account";
+import Checkout from "./pages/checkout";
+import ContactUs from "./pages/contact-us";
+import Login from "./pages/login";
+import Policy from "./pages/policy";
+import ProductDetails from "./pages/product-details";
+import ResetPassword from "./pages/reset-password";
+import Shop from "./pages/shop";
+import ShoppingCart from "./pages/shopping-cart";
+import Support from "./pages/support";
 const NotFoundPage = lazy(() => import("./pages/404"));
 
 function App() {
@@ -11,6 +27,22 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/account-order" element={<AccountOrder />} />
+        <Route path="/account-payment" element={<AccountPayment />} />
+        <Route path="/account-profile" element={<AccountProfile />} />
+        <Route path="/account-tickets" element={<AccountTickets />} />
+        <Route path="/account-wishlist" element={<AccountWishlist />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/policy" element={<Policy />} />
+        <Route path="/product-details" element={<ProductDetails />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/support" element={<Support />} />
         <Route path="/user/profile" element={<UserProfile />} />
         <Route path="/user/order" element={<UserOrder />} />
         <Route path="*" element={<NotFoundPage />} />
